20fe434906ebfbac81ae79569484b19fa735bed3
seungjin <xeungjin@nate.com>
2026-03-25 13:15:11 +0900

    2026-03-25

--- b.py
+++ b.py
@@ -1,5 +1,14 @@
-a, b, c = map(int, input().split())
-print(a)
-print(b)
-print(c)
-print(a + b + c)
+snack = '꿀꽈배기는\n 너무\n 맛있어요.'
+print(snack)
+
+print('C:\\Users\\Nadocoding')
+
+
+
+
+
+# a, b, c = map(int, input().split())
+# print(a)
+# print(b)
+# print(c)
+# print(a + b + c)
---
+++ jungol9170.py
@@ -0,0 +1,8 @@
+name = 'Kim'
+age = 15
+print(f'{name} is {age} years old.')
+
+## 1. -----------------------------------
+name = "Kim"
+age = 15
+print(name,'is', age, 'years old.')
