322640a3b828defdbe00fe0c3752960bc2439935
seungjin <xeungjin@nate.com>
2026-03-26 18:28:46 +0900

    jungol9264

---
+++ jungol9264.py
@@ -0,0 +1,16 @@
+# 3. --------------------
+N = int(input())
+for i in range (2, N + 1):
+    if i % 2 == 0:
+        print(i, end=' ')
+
+# 2. --------------------
+# N=int(input())
+# print(*range(2,N+1,2))
+
+# 1. --------------------
+# N = int(input())
+# # print(N)
+
+# for x in range(2, N+1, 2):
+#     print(x, end=' ')
