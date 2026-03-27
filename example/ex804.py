c23619e01b30239df01170de3c1d9137f2a4fc55
seungjin <xeungjin@nate.com>
2026-03-27 12:08:04 +0900

    j9258

---
+++ example/ex804.py
@@ -0,0 +1,6 @@
+# 2, 4, 6, 8, 10
+for x in range(1, 11):
+    if x % 2 == 1:
+        continue
+    print(x)
+            
---
+++ jungol/j9257.py
@@ -0,0 +1,9 @@
+# 3. --------------------
+print(" ".join(f"{x}" for x in range(1, 5001)))
+
+# 2. --------------------
+[print(i, end=" ") for i in range(1, 5001)]
+
+# 1. --------------------
+# for x in range(1, 5001):
+#     print(x, end=' ')
