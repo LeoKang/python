41752c56f49782a59f8d72a23c940d53378ebffd
seungjin <xeungjin@nate.com>
2026-04-07 13:30:59 +0900

    oracle connect

---
+++ jungol/j9290.py
@@ -0,0 +1,15 @@
+N = int(input())
+# print(N)
+i = 0
+while True:
+    if i >= N:
+        break
+    print(f"Python {i}")
+    i += 1
+
+# 1. -------------------------
+N = int(input())
+print(N)
+
+for i in range(N):
+    print("Python " + str(i))
---
+++ jungol/j9291.py
@@ -0,0 +1,18 @@
+print(input()*20)
+
+# 2. ----------------------
+trump = input()
+# print(trump)
+i = 0
+while i < 20:
+    print(trump, end='')
+    i += 1
+print()
+
+# 1. ---------------------
+ch = input()
+#print(ch)
+#print(ch[0])
+
+for i in range(20):
+    print(ch[0], end='')
