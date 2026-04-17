cc0bb6603d9a7b44cb24f9204412379b510e3b16
seungjin <xeungjin@nate.com>
2026-04-17 09:33:15 +0900

    9355

--- baekjoon/b8958.py
+++ baekjoon/b8958.py
@@ -1,4 +1,48 @@
+N=int(input())
+
+for _ in range(N):
+    OXshow=list(input())
+    Q=0
+    coin=0
+    for i in range(len(OXshow)):
+        if OXshow[i] == "O" :
+            coin+=1
+        else:
+            coin=0
+        Q+=coin
+    print(Q)
+# 2. ---------------------------------
+T = int(input())
+
+for i in range(T):
+    ox_res = input()
+    total_score = 0
+    current_line = 0
+    
+    for char in ox_res:
+        match char:
+            case 'O':
+                current_line += 1
+                total_score += current_line
+            case 'X':
+                current_line = 0
+            case _:
+                pass
+    print(total_score)
+
+# 1. ---------------------------
 T = int(input())
 for x in range(T):
-    str = input()
-    print(str)
+    str = input()   # OOXXOXXOOO
+    # print(str)
+
+    sum = 0
+    score = 1
+    for i in str:
+        # print(i, end=' ')
+        if i=='O':
+            sum += score
+            score += 1
+        else:
+            score = 1
+    print(sum)
---
+++ jungol/j9355.py
@@ -0,0 +1,8 @@
+lst = []
+for i in range(5):
+    inp = int(input())
+    lst.append(inp)
+
+print(lst)
+for i in lst:
+    print(i, end=' ')
